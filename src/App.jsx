47918dacf938867340a87a45a69bb888528aa1d5
A-karkaih <akarkaih@gmail.com>
2023-10-16 18:06:27 +0100

    The Experience Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import styles from './App.module.css';
 import About from './cpmponents/About/About';
+import Exprience from './cpmponents/Experience/Exprience';
 import Hero from './cpmponents/Hero/Hero';
 import Navbar from './cpmponents/Navbar/Navbar';
 
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Exprience />
     </div>
   
   )
